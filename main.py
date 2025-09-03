5edf7c90c18a5db3100d7d918eb42c6e84d2fee2
Rindra <hei.rindra.2@gmail.com>
2025-09-03 11:52:13 +0300

    add path post/cars

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+class Characteristic(BaseModel):
+    max_speed: float
+    max_fuel_capacity: float
+
+class car(BaseModel):
+    id: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+car_store: List[car] = []
+
+@app.post("/cars")
+def create_cars(car_payload: List[car]):
+    car_store.extend(car_payload)
+    car_as_json = []
+    for c in car_store:
+        car_as_json.append(c.model_dump())
+    return JSONResponse(content=car_as_json, status_code=201, media_type="application/json")
+
